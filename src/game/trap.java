c0b3304dbf5537d8063258e9232d71b86cb0df34
Aperture-Science-Employee-5705 <91751554+Aperture-Science-Employee-5705@users.noreply.github.com>
2021-12-04 14:12:18 +0000

    finished trap object

--- src/game/trap.java
+++ src/game/trap.java
@@ -1,6 +1,21 @@
 package game;
 
 public class trap {
+    private String name;
+    private String[] descriptions;//4 descriptions : upon activation ,during active ,upon disarm ,upon death
     private int damage;
+    private int turns;
     private String activationM;
+    private item deactivationM;
+    
+    public trap(String name ,String descriptions ,String activation ,int dmg ,int turns ,item deactivation) {
+        this.name = name;
+        this.descriptions = descriptions.split(";");
+        this.activationM = activation;
+        this.damage = dmg;
+        this.turns = turns;
+        this.deactivationM = deactivation;
+    }
+    public getData() {
+        return this.name ,this.descriptions ,this.activationM ,this.damage ,this.turns ,this.deactivationM;
 }
